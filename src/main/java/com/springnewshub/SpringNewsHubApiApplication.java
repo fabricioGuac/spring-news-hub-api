99e11ff457a9e4daa706a069c0a52247aa93b9d5
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-09-03 16:58:33 -0700

    initializes the project with the files from spring initializr

---
+++ src/main/java/com/springnewshub/SpringNewsHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.springnewshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewsHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNewsHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springnewshub/SpringNewsHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springnewshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNewsHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
