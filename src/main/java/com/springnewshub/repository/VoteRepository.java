fca403f3bd6654d2bbf0ceaefe32b14d77c3b81f
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-09-08 20:04:29 -0700

    delves in the vote repository interface

--- src/main/java/com/springnewshub/repository/VoteRepository.java
+++ src/main/java/com/springnewshub/repository/VoteRepository.java
@@ -1,5 +1,14 @@
 package com.springnewshub.repository;
 
-public interface VoteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.springnewshub.model.Vote;
+
+public interface VoteRepository extends JpaRepository<Vote, Integer>{
+
+	// Custom query method defined by Spring Data JPA to count the number of votes for a specific post
+	@Query("SELECT count(*) FROM Vote v where v.postId = :id")
+	int countVotesByPostId(@Param("id") Integer id);
 }
