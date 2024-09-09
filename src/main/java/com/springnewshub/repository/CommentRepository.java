a75cf9206119130c0f41a16dcc5858f03b47ac13
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-09-08 19:52:06 -0700

    delves in the comment repository interface

--- src/main/java/com/springnewshub/repository/CommentRepository.java
+++ src/main/java/com/springnewshub/repository/CommentRepository.java
@@ -1,5 +1,16 @@
 package com.springnewshub.repository;
 
-public interface CommentRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springnewshub.model.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer>{
+
+	// Custom query method interpreted by Spring Data JPA to retrieve all comments by a specific post ID
+	List<Comment> findAllCommentsByPostId(int postId) throws Exception;// Replace the parameter type?
+	
 }
