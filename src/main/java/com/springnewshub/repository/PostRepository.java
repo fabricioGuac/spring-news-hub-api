3472157d7a72bd48859af5c37f47cf65776b5aae
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-09-08 19:38:05 -0700

    delves in the post repository interface

--- src/main/java/com/springnewshub/repository/PostRepository.java
+++ src/main/java/com/springnewshub/repository/PostRepository.java
@@ -1,5 +1,15 @@
 package com.springnewshub.repository;
 
-public interface PostRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springnewshub.model.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository <Post, Integer>{
+
+	List<Post> findAllPostByUserId(Integer id) throws Exception;
+	
 }
